aea02a745a8b345def527f71280c52ffc3beef30
TheKanjiIsland <kashiananadeem@gmail.com>
2026-01-12 12:48:20 +0000

    Creating the question bank

---
+++ Code.py
@@ -0,0 +1,29 @@
+question_bank = [
+    (
+        "What is the capital of France?",
+        ["London", "Paris", "Berlin", "Madrid"],
+        "2"
+    ),
+    (
+        "Which data structure uses key-value pairs?",
+        ["List", "Tuple", "Dictionary" "Set"],
+        "3"
+    ),
+    (
+        "What is 5+7?",
+        ["10", "11", "12", "13"],
+        "3"
+    ),
+    (
+        "Which planet is known as the Red Planet?",
+        ["Earth", "Mars", "Jupiter", "Venus"],
+        "2"
+    ),
+    (
+        "Which language is used to create web pages?",
+        ["Python", "HTML", "Java", "C++"],
+        "2"
+    )
+]
+
+print(question_bank)
