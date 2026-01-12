ca0848e67e70ddccb007a1cc5dc120600618e0e6
TheKanjiIsland <kashiananadeem@gmail.com>
2026-01-12 13:10:58 +0000

    Syntax and element separation
    Fixed Syntax error and separated the dictionary into adequately named elements

--- Code.py
+++ Code.py
@@ -1,29 +1,30 @@
 question_bank = [
-    (
-        "What is the capital of France?",
-        ["London", "Paris", "Berlin", "Madrid"],
-        "2"
-    ),
-    (
-        "Which data structure uses key-value pairs?",
-        ["List", "Tuple", "Dictionary" "Set"],
-        "3"
-    ),
-    (
-        "What is 5+7?",
-        ["10", "11", "12", "13"],
-        "3"
-    ),
-    (
-        "Which planet is known as the Red Planet?",
-        ["Earth", "Mars", "Jupiter", "Venus"],
-        "2"
-    ),
-    (
-        "Which language is used to create web pages?",
-        ["Python", "HTML", "Java", "C++"],
-        "2"
-    )
+    {
+        "question": "What is the capital of France?",
+        "options": ["London", "Paris", "Berlin", "Madrid"],
+        "answer": "2"
+    },
+    {
+        "question": "Which data structure uses key-value pairs?",
+        "options": ["List", "Tuple", "Dictionary", "Set"],
+        "answer": "3"
+    },
+    {
+        "question": "What is 5 + 7?",
+        "options": ["10", "11", "12", "13"],
+        "answer": "3"
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Earth", "Mars", "Jupiter", "Venus"],
+        "answer": "2"
+    },
+    {
+        "question": "Which language is used to create web pages?",
+        "options": ["Python", "HTML", "Java", "C++"],
+        "answer": "2"
+    }
 ]
+
 
 print(question_bank)
