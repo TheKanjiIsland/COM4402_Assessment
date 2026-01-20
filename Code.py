846dfb05542d4497a37d3769934d84f89d36b1d9
TheKanjiIsland <kashiananadeem@gmail.com>
2026-01-20 08:59:01 +0000

    Rewriting dictionaries
    I separated the question bank dictionary into three separate ones to be more similar to the pseudocode design

--- Code.py
+++ Code.py
@@ -1,37 +1,31 @@
 # Implementing the question_bank
 
-question_bank = [
-    {
-        "question": "What is the capital of France?",
-        "options": ["London", "Paris", "Berlin", "Madrid"],
-        "answer": "2"
-    },
-    {
-        "question": "Which data structure uses key-value pairs?",
-        "options": ["List", "Tuple", "Dictionary", "Set"],
-        "answer": "3"
-    },
-    {
-        "question": "What is 5 + 7?",
-        "options": ["10", "11", "12", "13"],
-        "answer": "3"
-    },
-    {
-        "question": "Which planet is known as the Red Planet?",
-        "options": ["Earth", "Mars", "Jupiter", "Venus"],
-        "answer": "2"
-    },
-    {
-        "question": "Which language is used to create web pages?",
-        "options": ["Python", "HTML", "Java", "C++"],
-        "answer": "2"
-    }
-]
+questions = {
+    0: "What is the capital of France?",
+    1: "Which data structure uses key-value pairs?",
+    2: "What is 5 + 7?",
+    3: "Which planet is known as the Red Planet?",
+    4: "Which language is used to create web pages?"
+}
+
+options = {
+    0: ["London", "Paris", "Berlin", "Madrid"],
+    1: ["List", "Tuple", "Dictionary", "Set"],
+    2: ["10", "11", "12", "13"],
+    3: ["Earth", "Mars", "Jupiter", "Venus"],
+    4: ["Python", "HTML", "Java", "C++"]
+}
+
+answers = {
+    0: "2",
+    1: "3",
+    2: "3",
+    3: "2",
+    4: "2"
+}
 
 # Implementing other useful variables
 
 score = 0
 question_counter = 0
-total_questions = len(question_bank)
-
-print(question_bank)
+total_questions = len(questions)
